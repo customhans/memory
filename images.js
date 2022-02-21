df3148048316562b71c871425a084eb4c2d93a1f
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2022-02-21 23:05:33 +0100

    added nudes

--- images.js
+++ images.js
@@ -1,17 +1,14 @@
-var animals1 = [
-	"beluga", "bird1", "fish", "cham", "crab", "doggoe", "flamingo", "freedom", "gato", "gorilla", "icebird", "owl" ]
+var animals1 = [ "beluga", "bird1", "fish", "cham", "crab", "doggoe", "flamingo", "freedom", "gato", "gorilla", "icebird", "owl" ]
 
-var mountains = [
-	"desert", "devilstower", "table", "iceberg", "kirkjufell", "kirkjufell2", "matterhorn", "capitan", "sentinel", "kailash", "unknown", "zinnen" ]
+var mountains = [ "desert", "devilstower", "table", "iceberg", "kirkjufell", "kirkjufell2", "matterhorn", "capitan", "sentinel", "kailash", "unknown", "zinnen" ]
 
-var fungi = [
-	"birch", "boletus", "fancyone", "fly1", "fly2", "hat", "leetle1", "leetle2", "marone", "morel", "stone", "tintling" ]
+var fungi = [ "birch", "boletus", "fancyone", "fly1", "fly2", "hat", "leetle1", "leetle2", "marone", "morel", "stone", "tintling" ]
 
-var reptiles = [
-	"cham1", "snake", "turtle", "basilisk", "iguana", "snake3", "turtle2", "cham2", "turtle3", "gecko", "snake2", "snake4" ]
+var reptiles = [ "cham1", "snake", "turtle", "basilisk", "iguana", "snake3", "turtle2", "cham2", "turtle3", "gecko", "snake2", "snake4" ]
 
-var cats = [
-	"blackcat", "lynx", "cat2", "cheetah", "feline", "leopard", "lion", "manul", "panther", "snowleopard", "sortalynx", "tiger" ]
+var cats = [ "blackcat", "lynx", "cat2", "cheetah", "feline", "leopard", "lion", "manul", "panther", "snowleopard", "sortalynx", "tiger" ]
+
+var nudibranchs = [ "nudi1", "nudi2", "nudi3", "nudi4", "nudi5", "nudi6", "nudi7", "nudi8", "nudi9", "nudi10", "nudi11", "nudi12" ]
 
 var allRandom = []
 
