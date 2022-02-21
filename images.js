0c913165ad31b807f2c7e348f092ca359119b068
Hannes Flaschenträger <flaschentraeger@realitymaps.de>
2022-02-21 16:23:21 +0100

    added fungi

--- images.js
+++ images.js
@@ -7,3 +7,9 @@ var mountains = [
 	"desert", "devilstower", "table", "iceberg", "kirkjufell", "kirkjufell2", "matterhorn", "capitan", "sentinel", "kailash", "unknown", "zinnen",
 	"desert", "devilstower", "table", "iceberg", "kirkjufell", "kirkjufell2", "matterhorn", "capitan", "sentinel", "kailash", "unknown", "zinnen"
 ]
+
+var fungi = [
+	"birch", "boletus", "fancyone", "fly1", "fly2", "hat", "leetle1", "leetle2", "marone", "morel", "stone", "tintling",
+	"birch", "boletus", "fancyone", "fly1", "fly2", "hat", "leetle1", "leetle2", "marone", "morel", "stone", "tintling"
+]
+
